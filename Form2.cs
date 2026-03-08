ef0e818b054fb67f33fcad59ae7c0670043c8467
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-08 22:01:51 +0200

    le deuxieme formulaire

---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestioin_frais_academiques
+{
+    public partial class inscription : Form
+    {
+        public inscription()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
