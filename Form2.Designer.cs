a6fef4701aea9567d8a4253966baa97ba003354d
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-08 22:02:24 +0200

    le design pour le formulaire d'inscription

---
+++ Form2.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace gestioin_frais_academiques
+{
+    partial class inscription
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.fonction = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.create_account = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CREER UN COMPTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Entrer votre nom";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.create_account);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.fonction);
+            this.panel1.Controls.Add(this.nom);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(12, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 362);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(313, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ajouter un mot de passe de sécurité";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(159, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Donner votre fonction";
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(163, 81);
+            this.nom.Name = "nom";
+            this.nom.ShortcutsEnabled = false;
+            this.nom.Size = new System.Drawing.Size(384, 29);
+            this.nom.TabIndex = 4;
+            // 
+            // fonction
+            // 
+            this.fonction.Location = new System.Drawing.Point(163, 151);
+            this.fonction.Name = "fonction";
+            this.fonction.ShortcutsEnabled = false;
+            this.fonction.Size = new System.Drawing.Size(384, 29);
+            this.fonction.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(384, 29);
+            this.textBox1.TabIndex = 6;
+            // 
+            // create_account
+            // 
+            this.create_account.BackColor = System.Drawing.Color.Blue;
+            this.create_account.ForeColor = System.Drawing.Color.White;
+            this.create_account.Location = new System.Drawing.Point(376, 286);
+            this.create_account.Name = "create_account";
+            this.create_account.Size = new System.Drawing.Size(171, 50);
+            this.create_account.TabIndex = 7;
+            this.create_account.Text = "Créer un compte";
+            this.create_account.UseVisualStyleBackColor = false;
+            // 
+            // inscription
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "inscription";
+            this.Text = "inscription";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox fonction;
+        private System.Windows.Forms.Button create_account;
+    }
+}
