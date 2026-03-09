995a1efefe9f7ec8ac60d2f7863cd8f01420d80c
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-09 14:20:47 +0200

    la classe pour les utilisateurs

---
+++ utilisateur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestioin_frais_academiques
+{
+    internal class utilisateur
+    {
+            public int IdUser { get; set; }
+            public string Nom { get; set; }
+            public string Fonction { get; set; }
+            public string MotDePasse { get; set; }
+    }
+}
