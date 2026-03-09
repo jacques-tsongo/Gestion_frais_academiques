be5012dd54da5b49dc3ab7dea1faf836718ef2b0
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-09 14:19:12 +0200

    le form de paiement

---
+++ Form5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestioin_frais_academiques
+{
+    public partial class paiement : Form
+    {
+        public paiement()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
