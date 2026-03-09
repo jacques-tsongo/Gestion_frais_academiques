d9f2b9df080f7916a375d732884760a6e73fff3d
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-09 13:15:50 +0200

    la classe de connxion a la db

---
+++ connexionDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestioin_frais_academiques
+{
+    internal class connexionDB
+    {
+        private SqlConnection con;
+
+        public SqlConnection GetConnexion()
+        {
+            string chaine = @"Data Source=.;Initial Catalog=gestion_frais;Integrated Security=True";
+
+            con = new SqlConnection(chaine);
+
+            return con;
+        }
+
+    }
+}
