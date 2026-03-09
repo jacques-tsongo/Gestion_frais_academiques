38a561129e439c128d58646994e53e7c8c3fbd00
jacques-tsongo <kaserekatsongojacques2023@gmail.com>
2026-03-09 14:19:32 +0200

    le design pour le paiement

---
+++ Form5.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace gestioin_frais_academiques
+{
+    partial class paiement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.noms = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.matricule = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.filiere = new System.Windows.Forms.ComboBox();
+            this.promotion = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.devise = new System.Windows.Forms.ComboBox();
+            this.enrg_paiement = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Noms";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // noms
+            // 
+            this.noms.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noms.Location = new System.Drawing.Point(174, 23);
+            this.noms.Name = "noms";
+            this.noms.Size = new System.Drawing.Size(427, 26);
+            this.noms.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Snow;
+            this.panel1.Controls.Add(this.enrg_paiement);
+            this.panel1.Controls.Add(this.devise);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.promotion);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.filiere);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.matricule);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.noms);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(54, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(671, 382);
+            this.panel1.TabIndex = 2;
+            // 
+            // matricule
+            // 
+            this.matricule.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.matricule.Location = new System.Drawing.Point(174, 69);
+            this.matricule.Name = "matricule";
+            this.matricule.Size = new System.Drawing.Size(427, 26);
+            this.matricule.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Matricule";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Filiere";
+            // 
+            // filiere
+            // 
+            this.filiere.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filiere.FormattingEnabled = true;
+            this.filiere.Location = new System.Drawing.Point(174, 114);
+            this.filiere.Name = "filiere";
+            this.filiere.Size = new System.Drawing.Size(427, 28);
+            this.filiere.TabIndex = 5;
+            // 
+            // promotion
+            // 
+            this.promotion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.promotion.FormattingEnabled = true;
+            this.promotion.Location = new System.Drawing.Point(174, 161);
+            this.promotion.Name = "promotion";
+            this.promotion.Size = new System.Drawing.Size(427, 28);
+            this.promotion.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Promotion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(107, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Montant à payer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(41, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(458, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "La dévise";
+            // 
+            // devise
+            // 
+            this.devise.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.devise.FormattingEnabled = true;
+            this.devise.Location = new System.Drawing.Point(383, 238);
+            this.devise.Name = "devise";
+            this.devise.Size = new System.Drawing.Size(260, 28);
+            this.devise.TabIndex = 11;
+            // 
+            // enrg_paiement
+            // 
+            this.enrg_paiement.BackColor = System.Drawing.Color.Blue;
+            this.enrg_paiement.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enrg_paiement.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.enrg_paiement.Location = new System.Drawing.Point(174, 298);
+            this.enrg_paiement.Name = "enrg_paiement";
+            this.enrg_paiement.Size = new System.Drawing.Size(391, 55);
+            this.enrg_paiement.TabIndex = 12;
+            this.enrg_paiement.Text = "Enregistrer le paiement";
+            this.enrg_paiement.UseVisualStyleBackColor = false;
+            // 
+            // paiement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "paiement";
+            this.Text = "Paiement";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox noms;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox matricule;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox devise;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox promotion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox filiere;
+        private System.Windows.Forms.Button enrg_paiement;
+    }
+}
